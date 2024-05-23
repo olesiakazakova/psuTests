8abf5085bf7fe0c02c304c59a13f52295fc31cbf
olesiakazakova <145480358+olesiakazakova@users.noreply.github.com>
2024-05-23 19:05:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sqlite3
 import os
 import codecs
-from flask import Flask, render_template, url_for, request, session, redirect, abort, g, flash, make_response #импорт классов
+from flask import Flask, render_template, url_for, request, session, redirect, abort, g, flash, make_response
 from FDataBase import FDataBase
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from UserLogin import UserLogin
+import re
 
-DATABASE = '/tmp/flsite.db'
+
+
+DATABASE = 'flsite.db'
 DEBUG = True
 SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
 
@@ -65,203 +68,46 @@ def close_db(error):
 def index():
     return render_template('index.html', menu = dbase.getMenu(), post=dbase.getPostsAnonce(current_user.get_id()), title= 'psuTests')
 
+@app.route("/help")
+def help():
+    if current_user.is_authenticated:
+        return render_template('help2.html', menu=dbase.getMenu(), title='Инструкция')
 
-def simple_test(ans):
-    line = ""
-    itog = ""
-    var_answers = "абвгдежзийклмнопрстуфхцычшщая"
-    line = ans.readline()
-    while True:
-        line = ans.readline()
+    return render_template('help.html', menu = dbase.getMenu(), title= 'Инструкция')
 
-        if not line:
-            break
+def process_text(text):
 
-        if line[0].lower() in var_answers and line[1] == ")" and line[-2] == "*":
-            itog += line[0] + line[1] + "*\n"
-        else:
-            itog += line[0] + line[1] + "\n"
+    def process_line(line):
+        line = line.strip()
 
-    itog = itog.rstrip()
-
-    return itog
-
-
-def vstavka_slova_predlozhenia(ans):
-    itog = ""
-    while True:
-        line = ans.readline()
-
-        if not line:
-            break
-        if line[0] == "[" and (line[-1] == "]" or line[-2] == "]"):
-            if line[-2] == "]":
-                for simv in range(1, len(line) - 2):
-                    itog += line[simv]
-            else:
-                for simv in range(1, len(line) - 1):
-                    itog += line[simv]
-
-    return itog
-
-
-def sootv_func(ans):
-    itog = {}
-    simv = 0
-
-    while True:
-        first_word = ""
-        second_word = ""
-        line = ans.readline()
-
-        if not line:
-            break
 
         if "==" in line:
-            for simv in range(len(line)):
-                if line[simv] == "=":
-                    simv += 2
-                    break
-                first_word += line[simv]
+            return line
+        elif re.match(r"^\d+[)]\s*\w+", line):
 
-            if line[-1] == "\n":
-                for simv1 in range(simv, len(line) - 1):
-                    second_word += line[simv1]
+            return line
+        elif re.match(r"[а-яА-Я][)]\s*\w+", line):
+
+
+            match = re.match(r"^[а-яА-Я][)]\s*[а-яА-Я0-9,\s,\-,\–]*\*+$", line)
+
+            if match:
+                return line.strip()[:-1]
             else:
-                for simv1 in range(simv, len(line)):
-                    second_word += line[simv1]
-
-            itog[first_word] = second_word
-
-    return itog
+                return line[0] + ")"
+        elif re.match(r"^\[.*\]$", line):
+            return line.strip("[]")
+        return None
 
 
-##############################################
-def vstav():
-    questvst = open("quest.txt", "r", encoding="utf-16")
-
-    linevst = questvst.readline()
-
-    if linevst[2] == "в" and linevst[3] == "с":
-        itog.write(linevst)
-        questvst.close()
-        questvst = open("quest.txt", "r", encoding="utf-16")
-        vst = vstavka_slova_predlozhenia(questvst)
-        itog.write(vst)
-    else:
-        questvst.close()
+    lines = text.split("\n")
+    processed_lines = [process_line(line) for line in lines]
+    processed_lines = [line for line in processed_lines if line != None]
 
 
-def smpl():
-    questsmpl = open("quest.txt", "r", encoding="utf-16")
-    linesmpl = questsmpl.readline()
+    result =  "\n".join(processed_lines)
 
-    if len(linesmpl) >= 4 and linesmpl[2].lower() == "в" and linesmpl[3].lower() == "ы":
-        itog.write(linesmpl)
-        questsmpl.close()
-        questsmpl = open("quest.txt", "r", encoding="utf-16")
-        smpl = simple_test(questsmpl)
-        itog.write(smpl)
-    else:
-        questsmpl.close()
-
-
-def sootv():
-    questsootv = open("quest.txt", "r", encoding="utf-16")
-    linesootv = questsootv.readline()
-
-    if len(linesootv) >= 4 and linesootv[2].lower() == "у" and linesootv[3].lower() == "с":
-        itog.write(linesootv)
-        questsootv.close()
-        questsootv = open("quest.txt", "r", encoding="utf-16")
-        sotv = sootv_func(questsootv)
-        for i, (key, value) in enumerate(sotv.items()):
-            if i == len(sotv) - 1:
-                itog.write(f"{key} == {value}")
-            else:
-                itog.write(f"{key} == {value}\n")
-
-    else:
-        questsootv.close()
-
-    ###############################################
-
-
-# a = open("example_simple.txt", "r",encoding = "utf-8")
-# g = simple_test(a)
-# print (g)
-##print("------------------------")
-##a.close()
-##
-##
-# a = open("example_vstavka.txt", "r",encoding = "utf-8")
-# b=vstavka_slova_predlozhenia(a)
-# print(b)
-##a.close()
-##print("------------------------")
-##a = open("example_sootv.txt", "r",encoding = "utf-8")
-##c = sootv(a)
-##print(c.items())
-
-def parse(text):
-    global itog
-    test = open("full_test.txt", "w", encoding="utf-8")
-    test.write(text)
-    test.close()
-    test = open("full_test.txt", "r", encoding="utf-8")
-    quest = open("quest.txt", "w+", encoding="utf-16")
-    itog = open("itog.txt", "w+", encoding="utf-16")
-    k = 1
-    g = 0
-    line = ""
-
-    while True:
-        g += 1
-        line = test.readline()
-        if not line:
-            break
-
-        print(g)
-        if line[0].isdigit() and line[1] == ")"  and k != 1:
-            quest.close()
-            vstav()
-            smpl()
-            sootv()
-            itog.write("\n")
-
-            quest.close()
-            open("quest.txt", "w+", encoding="utf-16").close()
-            quest = open("quest.txt", "w+", encoding="utf-16")
-            quest.write(line)
-            k = 1
-        else:
-            quest.write(line)
-            k = 0
-    # Открываем файл в режиме чтения и записи ('rb+' для бинарного режима)
-    quest.close()
-    vstav()
-    smpl()
-    sootv()
-    itog.write("\n")
-    quest.close()
-    itog.close()
-    itog = open("itog.txt", "r", encoding="utf-16")
-    p = itog.read()
-    itog.close()
-    print(g)
-    # quest = open("quest.txt","w+",encoding = "utf-16")
-    # d = simple_test(quest)
-    # itog.write(d)
-    # print(d)
-
-    # vstav()
-
-    # g = str(input())
-    test.close()
-    os.remove("full_test.txt")
-    os.remove("quest.txt")
-    os.remove("itog.txt")
-    return p
+    return result
 
 
 @app.route("/upload", methods=["POST", "GET"])
@@ -269,7 +115,7 @@ def parse(text):
 def upload():
     if request.method == 'POST':
         if len(request.form['post']) > 10:
-            post=parse(request.form['post'])
+            post=process_text(request.form['post'])
             res = dbase.addPost(current_user.get_id(),request.form['classes'], request.form['themes'], post)
             if not res:
                 flash("Ошибка добавления заданий", "error")
@@ -339,5 +185,5 @@ def profile():
 def pageNotFount(error):
     return render_template('page404.html', title="Страница не найдена", menu=dbase.getMenu()), 404
 
-if __name__ == "__main__" : 
-    app.run(debug=True) #после завершения написания сайта поменять на false!!! (чтобы ошибки не были видны пользователю)
+if __name__ == "__main__" :
+    app.run(debug=True) #после завершения написания сайта поменять на false!!!
